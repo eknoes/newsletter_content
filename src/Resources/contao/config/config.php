e2e2e528a0765e36d6d471a3d1dc9fa98c1b31af
Soenke Huster <me@eknoes.de>
2019-11-14 11:55:33 +0200

    Moved classes according to Symfony

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -21,7 +21,7 @@
  */
 $GLOBALS['BE_MOD']['content']['newsletter']['tables'][] = 'tl_content';
 $GLOBALS['BE_MOD']['content']['newsletter']['send'] = array('DavidEnke\NewsletterContentBundle\Classes\NewsletterContent', 'send');
-//$GLOBALS['BE_MOD']['content']['newsletter']['stats'] = array('DavidEnke\NewsletterContentBundle\Classes\NewsletterStatistics', 'show');
+$GLOBALS['BE_MOD']['content']['newsletter']['stats'] = array('DavidEnke\NewsletterContentBundle\Classes\NewsletterStatistics', 'show');
 
 
 /**
@@ -58,4 +58,4 @@ $GLOBALS['TL_MODELS']['tl_newsletter_tracking'] = 'DavidEnke\NewsletterContentBu
 /**
  * Hooks
  */
-#$GLOBALS['TL_HOOKS']['outputBackendTemplate'][] = array('DavidEnke\NewsletterContentBundle\Classes\NewsletterStatistics', 'clean');
+$GLOBALS['TL_HOOKS']['outputBackendTemplate'][] = array('DavidEnke\NewsletterContentBundle\Classes\NewsletterStatistics', 'clean');
