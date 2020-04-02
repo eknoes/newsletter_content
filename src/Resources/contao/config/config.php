b6df28536b918afacd406bad094b6345e2165ea7
Soenke Huster <me@eknoes.de>
2020-04-02 16:19:39 +0200

    Fix contao hook

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -58,4 +58,4 @@ $GLOBALS['TL_MODELS']['tl_newsletter_tracking'] = 'DavidEnke\NewsletterContentBu
 /**
  * Hooks
  */
-$GLOBALS['TL_HOOKS']['outputBackendTemplate'][] = [DavidEnke\NewsletterContentBundle\Classes\NewsletterStatistics::class, 'clean'];
+$GLOBALS['TL_HOOKS']['outputBackendTemplate'][] = ['NewsletterStatistics', 'clean'];
